1be24459d2887af337e8e7926d4d0dbfd9aaa351
Gareth Cassin <nissacjg@gmail.com>
2018-07-03 22:59:23 +0100

    Day 5 - CSS Flex Box Gallery

---
+++ 05 - Flex Panel Gallery/app.js
@@ -0,0 +1,22 @@
+const panels = document.querySelectorAll(".panel");
+
+function toggleOpen(){
+	this.classList.toggle("open");
+}
+
+function toggleActive(e){
+	if(e.propertyName.includes("flex")){
+		this.classList.toggle("open-active");
+	}
+}
+
+function toggleClose () {
+	if(this.classList("open")){
+		this.classList.remove("open");
+	}
+	
+}
+
+
+panels.forEach(panel => panel.addEventListener("click", toggleOpen, toggleClose));
+panels.forEach(panel => panel.addEventListener("transitionend", toggleActive));

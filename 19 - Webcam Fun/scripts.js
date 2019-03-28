f33763257e01b75b4e1d8a90af97a8c20415d52e
Gareth Cassin <nissacjg@gmail.com>
2019-03-28 18:18:37 +0000

    added filters for alter image

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -22,7 +22,14 @@ const setCanvasVideo = () => {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, videoWidth, videoHeight)
+    // grab pixels
     let pixels = ctx.getImageData(0, 0, videoWidth, videoHeight)
+    // change pixel effects
+    // pixels = redEffect(pixels)
+    // pixels = splitRGB(pixels)
+    pixels = greenScreen(pixels)
+    // put pixels back
+    ctx.putImageData(pixels, 0, 0)
     
   }, 16)
 }
@@ -42,7 +49,56 @@ const screenshot = () => {
   link.textContent = 'Download Image'
   link.innerHTML = `<img src='${data}' alt='screenshot' />`
   strip.insertBefore(link, strip.firstChild)
+}
 
+const redEffect = pixels => {
+  for (let i = 0; i < pixels.data.length; i += 4 ) {
+    // red
+    pixels.data[i] = pixels.data[i] + 100
+    // green
+    pixels.data[i + 1] = pixels.data[i + 1] - 50
+    // blue
+    pixels.data[i + 2] = pixels.data[i + 2] + 5
+  }
+  return pixels 
+}
+
+const splitRGB = pixels => {
+  for (let i = 0; i < pixels.data.length; i += 4 ) {
+    // red
+    pixels.data[i - 150] = pixels.data[i]
+    // green
+    pixels.data[i + 500] = pixels.data[i + 1]
+    // blue
+    pixels.data[i - 550] = pixels.data[i + 2]
+  }
+  return pixels 
+}
+
+const greenScreen = pixels => {
+  const levels  = {}
+
+  document.querySelectorAll('.rgb input').forEach(input => {
+    levels[input.name] = input.value
+  })
+
+  for (i = 0; i < pixels.data.length; i += 4) {
+    red = pixels.data[i]
+    green = pixels.data[i + 1]
+    blue = pixels.data[i + 2]
+    alpha = pixels.data[i + 3]
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+        // set alpha to 0 (so transparent)
+        pixels.data[i + 3] = 0
+      }
+  }
+  return pixels
 
 }
 
